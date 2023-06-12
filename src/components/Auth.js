6d7f210efaa884cd24707b4c7c8eab82fa184c8b
exit <exit>
2023-06-12 09:42:27 +0300

    Auth

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,12 +1,53 @@
-
+import {auth, googleProvider} from "../config/firebase";
+import { createUserWithEmailAndPassword, signInWithPopup, signOut} from "firebase/auth";
+import {useState} from "react"
 
 export const Auth = () => {
-  return (
-    <div>
-      <input placeholder="Email..."></input>
-      <input placeholder="Password..."></input>
-    </div>
-  )
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    console.log(auth?.currentUser?.email);
+
+    const signedIn = async() => {
+        try{
+            await createUserWithEmailAndPassword(auth, email, password)
+        }catch(err)
+        {
+            console.error(err)
+        }
+        
+    }
+
+    const signedInWithGoogle = async() => {
+        try{
+            await signInWithPopup(auth, googleProvider)
+        }catch(err)
+        {
+            console.error(err)
+        }
+
+    }
+
+    const logout = async() => {
+        try{
+            await signOut(auth)
+        }catch(err)
+        {
+            console.error(err)
+        }
+
+    }
+    return (
+        <div>
+            <input placeholder="Email..." onChange = {(e) => setEmail(e.target.value)}/> 
+            <input placeholder="Password..." type = "password" onChange = {(e) => setPassword(e.target.value)}/>
+            <button onClick = {signedIn}>Signed in</button>
+
+            <button onClick = {signedInWithGoogle}>Signed with Google</button>
+
+            <button onClick = {logout}>Log out</button>
+        </div>
+    )
 }
 
 export default Auth
