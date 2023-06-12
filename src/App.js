4ea57cc2e277e34dc87e02a6ef26bc409b322b39
exit <exit>
2023-06-12 22:28:51 +0300

    Working optimized

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import Auth from "./components/Auth";
 import Header from "./components/Header";
 import {auth, db, googleProvider} from "./config/firebase";
-import { createUserWithEmailAndPassword, signInWithPopup, signOut} from "firebase/auth";
-import { getDoc, collection, addDoc, doc} from "firebase/firestore";
+import { createUserWithEmailAndPassword, signInWithPopup, signOut, signInWithEmailAndPassword} from "firebase/auth";
+import { getDoc, collection, addDoc, doc, setDoc} from "firebase/firestore";
 import {useEffect,useState} from "react"
 import Tasks from "./components/Tasks";
 
@@ -11,23 +11,85 @@ function App() {
   const [user, setUser] = useState(null);
   const [tasks, setTasks] = useState([]);
  
-  useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged(async(user) => {
-      if (user) {
-        setUser(user);
-        const currentUserId = auth.currentUser.uid;
-        const documentRef = doc(collection(db, "users"), currentUserId);
+  // useEffect(() => {
+  //   const unsubscribe = auth.onAuthStateChanged(async(user) => {
+  //     if (user) {
+  //       setUser(user);
+  //       const currentUserId = auth.currentUser.uid;
+  //       const documentRef = doc(collection(db, "users"), currentUserId);
 
-        const documentSnapshot = await getDoc(documentRef)
-        setTasks(documentSnapshot.data().tasks)
-        // console.log(tasks)
-      } else {
-        setUser(null);
-        setTasks([])
-      }
-    }, []);
-  })
+  //       const documentSnapshot = await getDoc(documentRef)
+  //       setTasks(documentSnapshot.data().tasks)
+  //       // console.log(tasks)
+  //     } else {
+  //       setUser(null);
+  //       setTasks([])
+  //     }
+  //   }, []);
+  // })
+ 
+    
+    
+    const initTasksAndUser = async() => {
+      setUser(auth.currentUser);
+      const currentUserId = auth.currentUser.uid;
+      const documentRef = doc(collection(db, "users"), currentUserId);
 
+      const documentSnapshot = await getDoc(documentRef)
+      setTasks(documentSnapshot.data().tasks)
+      // console.log(tasks)
+    } 
+    const usercolection = collection(db, "users");
+    const addUser = async (email) => {
+        try{
+            const docRef = doc(usercolection, auth.currentUser.uid);
+            await setDoc(docRef,{
+                email: email,
+                tasks: []
+            })
+        }catch(err)
+        {
+            console.error(err)
+        }
+    }
+    const signedIn = async(email, password) => {
+        try{
+            console.log(email )
+            console.log(password)
+            console.log("HELLOOOOOO")
+            await createUserWithEmailAndPassword(auth, email, password)
+            addUser(email)
+            initTasksAndUser()
+        }catch(err)
+        {
+            console.error(err)
+        }
+        
+    }
+
+    const signedInWithGoogle = async() => {
+        try{
+            await signInWithPopup(auth, googleProvider).then((userCredential)=>{
+                addUser(userCredential.user.email)
+                initTasksAndUser()
+            })
+            
+        }catch(err)
+        {
+            console.error(err)
+        }
+
+    }
+
+    const login = async (email, password) =>{
+        try{
+            await signInWithEmailAndPassword(auth, email, password)
+            initTasksAndUser()
+        }catch(err)
+        {
+            console.error(err)
+        }
+    }
   const onAdd = () => {
     console.log("clicked");
   }
@@ -41,6 +103,8 @@ function App() {
           <button onClick = {async()=>{
               try{
                 await signOut(auth)
+                setTasks([]);
+                setUser(null);
             }catch(err)
             {
                 console.error(err)
@@ -50,7 +114,7 @@ function App() {
           <Header title = "Your Tasks" onClick={onAdd}/>
           
           {tasks.length === 0 ? <h3 >No tasks</h3> : <Tasks tasks = {tasks}/>}
-        </> :  <Auth />}
+        </> :  <Auth login = {login} signedIn = {signedIn} signedInWithGoogle = {signedInWithGoogle}/>}
 
 
     </div>
--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,70 +1,13 @@
 import {auth, db, googleProvider} from "../config/firebase";
-import { createUserWithEmailAndPassword, signInWithPopup, signOut, signInWithEmailAndPassword} from "firebase/auth";
-import { getDoc, collection, addDoc, setDoc, doc } from "firebase/firestore";
+
+
 import {useState} from "react"
 
-export const Auth = () => {
+export const Auth = ({login, signedIn, signedInWithGoogle}) => {
+    
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    
-    
-    
-    const usercolection = collection(db, "users");
-    const addUser = async (email) => {
-        try{
-            const docRef = doc(usercolection, auth.currentUser.uid);
-            await setDoc(docRef,{
-                email: email,
-                tasks: []
-            })
-        }catch(err)
-        {
-            console.error(err)
-        }
-    }
-    const signedIn = async() => {
-        try{
-            await createUserWithEmailAndPassword(auth, email, password)
 
-            addUser(email)
-        }catch(err)
-        {
-            console.error(err)
-        }
-        
-    }
-
-    const signedInWithGoogle = async() => {
-        try{
-            await signInWithPopup(auth, googleProvider).then((userCredential)=>{
-                addUser(userCredential.user.email)
-            })
-            
-        }catch(err)
-        {
-            console.error(err)
-        }
-
-    }
-
-    const login = async (email, password) =>{
-        try{
-            await signInWithEmailAndPassword(auth, email, password)
-        }catch(err)
-        {
-            console.error(err)
-        }
-    }
-
-    const logout = async() => {
-        try{
-            await signOut(auth)
-        }catch(err)
-        {
-            console.error(err)
-        }
-
-    }
     return (
         <div>
             <input placeholder="Email..." onChange = {(e) => {
@@ -75,11 +18,11 @@ export const Auth = () => {
                 setPassword(e.target.value)
                 console.log(auth?.currentUser?.email);
             }}/>
-            <button onClick = {signedIn}>Signed in</button>
+            <button onClick = {()=>{signedIn(email, password)}}>Signed in</button>
 
-            <button onClick = {signedInWithGoogle}>Signed with Google</button>
+            <button onClick = {()=>{signedInWithGoogle(email, password)}}>Signed with Google</button>
             <button onClick = {()=>{login(email,password)}}>Log in</button>
-            <button onClick = {logout}>Log out</button>
+            
         </div>
     )
 }
